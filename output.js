107ddd57494b0d4ad2c6d124b0f9923222f9dcdd
action <action@github.com>
2024-12-26 04:20:04 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Thu Dec 26 2024 04:17:26 GMT+0000 (Coordinated Universal Time)
+//Thu Dec 26 2024 04:20:04 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 var encode_version = "jsjiami.com.v5";
@@ -6,36 +6,25 @@ var Rnik = JSON.parse($response.body);
 Rnik = {
   message: "成功!",
   result: {
-    id: 13752115,
-    pageName: "付费页面",
-    elementId: "付费点击",
-    uid: "A9A675B1-3999-4DF2-81BA-08F23623CD92",
-    iosOrAnd: "ios",
-    version: "1.6.7",
-    isVip: 1,
-    isCN: 1,
-    deviceId: "26242316-0D4E-4F0F-A1B2-1FE5A99E68BC",
-    deviceType: "iPhone 11",
-    pageTitle: "首页我们的故事",
-    elementPosition: "",
-    elementContent: "付费页面",
-    ip: "101.84.134.133"
+    uid: "efe376d39b4f4385a45cb19ae0c8c13b",
+    phone: "18888888888",
+    memberExpire: 4092610661000
   },
   code: "00000"
 };
 $done({
   body: JSON.stringify(Rnik)
 });
-(function (_0x5924a6, _0x3f62d9, _0x25afe0) {
-  _0x25afe0 = "al";
+(function (_0x4f25fb, _0x35a1f4, _0x4de8ef) {
+  _0x4de8ef = "al";
   try {
-    _0x25afe0 += "ert";
-    _0x3f62d9 = encode_version;
-    if (!(typeof _0x3f62d9 !== "undefined" && _0x3f62d9 === "jsjiami.com.v5")) {
-      _0x5924a6[_0x25afe0]("删除版本号，js会定期弹窗，还请支持我们的工作");
+    _0x4de8ef += "ert";
+    _0x35a1f4 = encode_version;
+    if (!(typeof _0x35a1f4 !== "undefined" && _0x35a1f4 === "jsjiami.com.v5")) {
+      _0x4f25fb[_0x4de8ef]("删除版本号，js会定期弹窗，还请支持我们的工作");
     }
-  } catch (_0x5798d2) {
-    _0x5924a6[_0x25afe0]("删除版本号，js会定期弹窗");
+  } catch (_0x3d5a62) {
+    _0x4f25fb[_0x4de8ef]("删除版本号，js会定期弹窗");
   }
 })(window);
 encode_version = "jsjiami.com.v5";
