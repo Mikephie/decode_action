eec2de4934dc8ce6ac64f1ce3e93d37adabd5e81
action <action@github.com>
2025-01-06 13:31:17 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,35 +1,4 @@
-//Mon Jan 06 2025 12:42:46 GMT+0000 (Coordinated Universal Time)
+//Mon Jan 06 2025 13:31:16 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-var _0x164fb0 = 10;
-var _0xde71fe = $response.body;
-_0x164fb0 = 12;
-let _0x1e123d = JSON.parse($response.body);
-_0x1e123d = {
-  success: true,
-  result: {
-    remainTimeSeconds: 99999,
-    realnameStatus: "NO",
-    wordage: 7777777,
-    mobile: "https://t.me/GieGie777",
-    inviteCode: "000000",
-    vipGroupInfos: [{
-      groupType: "TYPE_ONE",
-      vipType: "VIP",
-      autoPay: "NO"
-    }],
-    type: "VIP",
-    vipExpireTime: "2777-07-07 07:07:07",
-    vipExpireDays: 99999999,
-    registerTime: "2022-09-09 03:20:32",
-    nickname: "https://t.me/GieGie777",
-    times: 77777777,
-    headImg: "https://zdimg.lifeweek.com.cn/app/20230410/16811146599505136.jpg",
-    dataId: "00000000000000000000"
-  },
-  returnCode: "200",
-  timeOut: false
-};
-$done({
-  body: JSON.stringify(_0x1e123d)
-});
+testing;
