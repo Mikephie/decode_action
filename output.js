94b34f0a050bcf91444aeb5b1d294ec89a3c967a
action <action@github.com>
2025-01-09 23:24:08 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Thu Jan 09 2025 13:27:31 GMT+0000 (Coordinated Universal Time)
+//Thu Jan 09 2025 23:24:07 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("酷我音乐");
