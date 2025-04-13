643b17f8841ea3e3af28bd95c324a81e1c9b1079
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-04-13 11:55:45 +0800

    Create optimize.js

---
+++ src/plugin/optimize.js
@@ -0,0 +1,24 @@
+import { parse } from '@babel/parser'
+import generator from '@babel/generator'
+import traverse from '@babel/traverse'
+import deleteUnreachableCode from '../visitor/delete-unreachable-code.js'
+import deleteNestedBlocks from '../visitor/delete-nested-blocks.js'
+import calculateConstantExp from '../visitor/calculate-constant-exp.js'
+import calculateRString from '../visitor/calculate-rstring.js'
+
+export function optimize(code) {
+  let ast
+  try {
+    ast = parse(code, { errorRecovery: true })
+  } catch (e) {
+    console.error(`optimize parse error: ${e.reasonCode}`)
+    return code
+  }
+
+  traverse(ast, deleteUnreachableCode)
+  traverse(ast, deleteNestedBlocks)
+  traverse(ast, calculateConstantExp)
+  traverse(ast, calculateRString)
+
+  return generator(ast).code
+}
