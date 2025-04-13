f0857fb19eeaa50634b9c4f0f27e05e52f0dc722
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-04-13 12:25:15 +0800

    Update eval.js

--- src/plugin/eval.js
+++ src/plugin/eval.js
@@ -1,69 +1,69 @@
-const { parse } = require('@babel/parser')
-const generator = require('@babel/generator').default
-const traverse = require('@babel/traverse').default
-const t = require('@babel/types')
-const { formatCode } = require('./common.js')
-const { optimize } = require('./optimize.js')
+import { parse } from '@babel/parser'
+import generator from '@babel/generator'
+import traverse from '@babel/traverse'
+import * as t from '@babel/types'
+import { formatCode } from './common.js'
+import { optimize } from './optimize.js'
 
-const name = 'eval'
+export const name = 'eval'
 
-function safeUnpack(code) {
-  let unpacked = ''
-  const fakeEval = function (input) {
-    unpacked = input
-    return input
-  }
-  try {
-    const modifiedCode = code.replace(/eval\s*\(/, 'fakeEval(')
-    const context = { fakeEval, String, RegExp }
-
-    const contextKeys = Object.keys(context)
-    const contextValues = Object.values(context)
-
-    const runner = new Function(...contextKeys, modifiedCode)
-    runner(...contextValues)
-
-    return unpacked
-  } catch {
-    return null
-  }
-}
-
-function recursiveUnpack(code, depth = 0) {
-  if (depth > 10) return code
-  const result = safeUnpack(code)
-  if (result && result !== code) {
-    if (result.includes('eval(')) {
-      return recursiveUnpack(result, depth + 1)
+export function handle(code) {
+  function safeUnpack(code) {
+    let unpacked = ''
+    const fakeEval = function (input) {
+      unpacked = input
+      return input
     }
-    return result
-  }
-  return code
-}
+    try {
+      const modifiedCode = code.replace(/eval\s*\(/, 'fakeEval(')
+      const context = { fakeEval, String, RegExp }
 
-function simpleUnpack(code) {
-  const ast = parse(code, { errorRecovery: true })
-  const lines = ast.program.body
-  let data = null
-  for (const line of lines) {
-    if (t.isEmptyStatement(line)) continue
-    if (data) return null
-    if (
-      t.isCallExpression(line?.expression) &&
-      line.expression.callee?.name === 'eval' &&
-      line.expression.arguments.length === 1 &&
-      t.isCallExpression(line.expression.arguments[0])
-    ) {
-      data = t.expressionStatement(line.expression.arguments[0])
-      continue
+      const contextKeys = Object.keys(context)
+      const contextValues = Object.values(context)
+
+      const runner = new Function(...contextKeys, modifiedCode)
+      runner(...contextValues)
+
+      return unpacked
+    } catch {
+      return null
     }
-    return null
   }
-  if (!data) return null
-  return eval(generator(data, { minified: true }).code)
-}
 
-function handle(code) {
+  function recursiveUnpack(code, depth = 0) {
+    if (depth > 10) return code
+    const result = safeUnpack(code)
+    if (result && result !== code) {
+      if (result.includes('eval(')) {
+        return recursiveUnpack(result, depth + 1)
+      }
+      return result
+    }
+    return code
+  }
+
+  function simpleUnpack(code) {
+    const ast = parse(code, { errorRecovery: true })
+    const lines = ast.program.body
+    let data = null
+    for (const line of lines) {
+      if (t.isEmptyStatement(line)) continue
+      if (data) return null
+      if (
+        t.isCallExpression(line?.expression) &&
+        line.expression.callee?.name === 'eval' &&
+        line.expression.arguments.length === 1 &&
+        t.isCallExpression(line.expression.arguments[0])
+      ) {
+        data = t.expressionStatement(line.expression.arguments[0])
+        continue
+      }
+      return null
+    }
+    if (!data) return null
+    return eval(generator.default(data, { minified: true }).code)
+  }
+
   try {
     const result = recursiveUnpack(code)
     if (result && result !== code) {
@@ -84,11 +84,4 @@ function handle(code) {
     return null
   }
 }
-
-module.exports = {
-  name,
-  handle
-}
-
-// 兼容 ES Module 导入
-module.exports.default = module.exports
+export default { name, handle }
