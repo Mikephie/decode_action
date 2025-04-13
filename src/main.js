ed9de81e6e412a2e1df14642e92ed80f0c7b0f35
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-04-13 12:10:58 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,91 +1,77 @@
-import fs from 'fs';
-import process from 'process';
+import fs from 'fs'
+import process from 'process'
 
-// 动态加载 ESM 插件
-const commonModule = await import('./plugin/common.js');
-const jjencodeModule = await import('./plugin/jjencode.js');
-const sojsonModule = await import('./plugin/sojson.js');
-const sojsonv7Module = await import('./plugin/sojsonv7.js');
-const obfuscatorModule = await import('./plugin/obfuscator.js');
-const awscModule = await import('./plugin/awsc.js');
-const jsconfuserModule = await import('./plugin/jsconfuser.js');
-const jsaaencodeModule = await import('./plugin/aaencode.js');
-const optimizeModule = await import('./plugin/optimize.js');
+// 动态导入插件
+const pluginList = [
+  'obfuscator',
+  'sojsonv7',
+  'sojson',
+  'jsconfuser',
+  'awsc',
+  'jjencode',
+  'aaencode',
+  'optimize', // AST 逻辑优化
+  'common'    // 格式美化 beautify
+]
 
-// 兼容 default / module.exports
-const PluginCommon = commonModule.default || commonModule;
-const PluginJjencode = jjencodeModule.default || jjencodeModule;
-const PluginSojson = sojsonModule.default || sojsonModule;
-const PluginSojsonV7 = sojsonv7Module.default || sojsonv7Module;
-const PluginObfuscator = obfuscatorModule.default || obfuscatorModule;
-const PluginAwsc = awscModule.default || awscModule;
-const PluginJsconfuser = jsconfuserModule.default || jsconfuserModule;
-const PluginAaencode = jsaaencodeModule.default || jsaaencodeModule;
-const PluginOptimize = optimizeModule.optimize || optimizeModule;
-
-// 命令行参数
-let encodeFile = 'input.js';
-let decodeFile = 'output.js';
+// 命令行参数处理
+let encodeFile = 'input.js'
+let decodeFile = 'output.js'
 
 for (let i = 2; i < process.argv.length; i += 2) {
   if (process.argv[i] === '-i') {
-    encodeFile = process.argv[i + 1];
+    encodeFile = process.argv[i + 1]
   } else if (process.argv[i] === '-o') {
-    decodeFile = process.argv[i + 1];
+    decodeFile = process.argv[i + 1]
   }
 }
 
-console.log(`输入: ${encodeFile}`);
-console.log(`输出: ${decodeFile}`);
+console.log(`输入: ${encodeFile}`)
+console.log(`输出: ${decodeFile}`)
 
-const sourceCode = fs.readFileSync(encodeFile, { encoding: 'utf-8' });
+const sourceCode = fs.readFileSync(encodeFile, { encoding: 'utf-8' })
 
-let processedCode = sourceCode;
-let pluginUsed = '';
-let time;
+let processedCode = sourceCode
+let pluginUsed = ''
+let time
 
-// 插件执行顺序（新增 optimize）
-const plugins = [
-  { name: 'obfuscator', plugin: PluginObfuscator },
-  { name: 'sojsonv7', plugin: PluginSojsonV7 },
-  { name: 'sojson', plugin: PluginSojson },
-  { name: 'jsconfuser', plugin: PluginJsconfuser },
-  { name: 'awsc', plugin: PluginAwsc },
-  { name: 'jjencode', plugin: PluginJjencode },
-  { name: 'aaencode', plugin: PluginAaencode },
-  { name: 'optimize', plugin: PluginOptimize },  // 优化 AST
-  { name: 'common', plugin: PluginCommon },      // 最后格式美化
-];
+for (const name of pluginList) {
+  if (sourceCode.indexOf('smEcV') !== -1) break
 
-for (const plugin of plugins) {
-  if (sourceCode.indexOf('smEcV') !== -1) break; // 特殊处理
+  const module = await import(`./plugin/${name}.js`)
+  const plugin = module.default || module
 
   try {
-    const code = plugin.plugin(sourceCode);
+    const code = plugin.handle
+      ? plugin.handle(sourceCode)  // 解包插件
+      : plugin.optimize
+        ? plugin.optimize(sourceCode)  // optimize 逻辑优化
+        : plugin.formatCode(sourceCode)  // common 格式美化
+
     if (code && code !== processedCode) {
-      processedCode = code;
-      pluginUsed = plugin.name;
-      break;
+      processedCode = code
+      pluginUsed = name
+      break
     }
-  } catch (error) {
-    console.error(`插件 ${plugin.name} 处理时发生错误: ${error.message}`);
+  } catch (e) {
+    console.error(`插件 ${name} 处理时发生错误: ${e.message}`)
   }
 }
 
 if (processedCode !== sourceCode) {
-  time = new Date();
+  time = new Date()
   const header = [
     `//${time}`,
     "//Base:https://github.com/echo094/decode-js",
     "//Modify:https://github.com/smallfawn/decode_action"
-  ].join('\n');
+  ].join('\n')
 
-  const outputCode = header + '\n' + processedCode;
+  const outputCode = header + '\n' + processedCode
 
   fs.writeFile(decodeFile, outputCode, (err) => {
-    if (err) throw err;
-    console.log(`使用插件 ${pluginUsed} 成功处理并写入文件 ${decodeFile}`);
-  });
+    if (err) throw err
+    console.log(`使用插件 ${pluginUsed} 成功处理并写入文件 ${decodeFile}`)
+  })
 } else {
-  console.log(`所有插件处理后的代码与原代码一致，未写入文件。`);
+  console.log(`所有插件处理后的代码与原代码一致，未写入文件。`)
 }
