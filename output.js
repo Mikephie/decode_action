b6a5035129423624a8d5fa6349eff476ec61da1e
action <action@github.com>
2025-04-14 04:31:09 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,115 +1,53 @@
-//Mon Apr 14 2025 00:51:18 GMT+0000 (Coordinated Universal Time)
-//Base:https://github.com/echo094/decode-js
-//Modify:https://github.com/smallfawn/decode_action
-let names = "DailyWallpaper";
-let productName = "raven_pro";
-let productType = "com.skyriser.bingwallpapers.removeads";
-let appVersion = null;
-let notifyState = true;
-let ua = true;
+// 2025-04-14T04:31:09.052Z
+// Base: https://github.com/echo094/decode-js
+// Modify: https://github.com/smallfawn/decode_action
+
+[rewrite_local] ^
+https ? : \/\/(cdn-bm|bmall)\.camera360\.com\/api\/(mix\/(getinfo|purchase|recovery)|iap\/check-receipt|mix-asset\/assets) url script-response-body https:/ / raw.githubusercontent.com / Mike - offers / Rewrite / refs / heads / master / QuantumultX / Mix.js
+https: \/\/mix-api\.camera360\.com\/v\d\/operational-positions url reject
+
+[mitm]
+hostname = cdn - bm.camera360.com, bmall.camera360.com, mix - api.camera360.com
+
+  **
+  ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** * /
+
+ﾟωﾟﾉ = /｀ｍ´）ﾉ ~┻━┻   / /*´∇｀*/
+
+/*************************************/
+
+// Mix & Mix2 解锁 VIP
+
 let obj = JSON.parse($response.body);
-let $ = new Env(names);
-obj.subscriber = {
-  non_subscriptions: {},
-  first_seen: "2024-03-08T04:44:30Z",
-  original_application_version: appVersion,
-  other_purchases: {
-    [productType]: {
-      price: {
-        amount: 0,
-        currency: "USD",
-      },
-      display_name: null,
-      purchase_date: "2024-03-08T04:44:44Z",
-    },
-  },
-  management_url: null,
-  subscriptions: {},
-  entitlements: {},
-  original_purchase_date: "2024-03-08T04:44:14Z",
-  original_app_user_id: "$RCAnonymousID:0400000000000000000000000000000",
-  last_seen: "2024-03-08T04:44:30Z",
-};
-obj.subscriber.non_subscriptions[productType] = [{
-  id: "aaaaaaaaaa",
-  is_sandbox: false,
-  price: {
-    amount: 0,
-    currency: "USD",
-  },
-  display_name: null,
-  purchase_date: "2024-03-08T04:44:44Z",
-  original_purchase_date: "2024-03-08T04:44:44Z",
-  store: "app_store",
-  store_transaction_id: "280000000000000",
-}, ];
-$.notify("XiaoMao_" + names + " 执行成功！", "", "Nice!已解锁成功，可关掉此脚本。", "https://i.pixiv.re/img-original/img/2022/12/19/00/06/12/103718184_p0.png");
+
+if (obj.data && obj.data.vipItem) {
+  // 设置 VIP 状态为永久
+  obj.data.vipItem.vipType = "subscription";
+  obj.data.vipItem.vipStatus = 1;
+  obj.data.vipItem.vipExpiry = 4092599349000; // 2099年过期时间
+}
+
+// 解锁所有 VIP 项目
+if (obj.data && obj.data.vipList) {
+  for (let item of obj.data.vipList) {
+    item.isValid = true;
+    item.vipType = "permanent";
+  }
+}
+
+// 解锁购买状态
+if (obj.data && obj.data.purchase) {
+  obj.data.purchase.hasPurchased = true;
+  obj.data.purchase.vipStatus = 1;
+}
+
+// 解锁已购资产
+if (obj.data && obj.data.assets) {
+  for (let asset of obj.data.assets) {
+    asset.isFree = true;
+  }
+}
+
 $done({
   body: JSON.stringify(obj)
 });
-
-function Env(name) {
-  const isLoon = typeof $loon !== "undefined";
-  const isSurge = typeof $httpClient !== "undefined" && !isLoon;
-  const isQX = typeof $task !== "undefined";
-  const read = (key) => {
-    if (isLoon || isSurge) return $persistentStore.read(key);
-    if (isQX) return $prefs.valueForKey(key)
-  };
-  const write = (key, value) => {
-    if (isLoon || isSurge) return $persistentStore.write(key, value);
-    if (isQX) return $prefs.setValueForKey(key, value)
-  };
-  const notify = (title = "XiaoMao", subtitle = "", message = "", url = "", url2 = url) => {
-    if (isLoon) $notification.post(title, subtitle, message, url);
-    if (isSurge) $notification.post(title, subtitle, message, {
-      url
-    });
-    if (isQX) $notify(title, subtitle, message, {
-      "open-url": url,
-      "media-url": url2
-    })
-  };
-  const get = (url, callback) => {
-    if (isLoon || isSurge) $httpClient.get(url, callback);
-    if (isQX) {
-      url.method = `GET`;
-      $task.fetch(url).then((resp) => callback(null, {}, resp.body))
-    }
-  };
-  const post = (url, callback) => {
-    if (isLoon || isSurge) $httpClient.post(url, callback);
-    if (isQX) {
-      url.method = `POST`;
-      $task.fetch(url).then((resp) => callback(null, {}, resp.body))
-    }
-  };
-  const put = (url, callback) => {
-    if (isLoon || isSurge) $httpClient.put(url, callback);
-    if (isQX) {
-      url.method = "PUT";
-      $task.fetch(url).then((resp) => callback(null, {}, resp.body))
-    }
-  };
-  const toObj = (str) => JSON.parse(str);
-  const toStr = (obj) => JSON.stringify(obj);
-  const queryStr = (obj) => {
-    return Object.keys(obj).map((key) => `${key}=${obj[key]}`).join("&")
-  };
-  const log = (message) => console.log(message);
-  const done = (value = {}) => $done(value);
-  return {
-    name,
-    read,
-    write,
-    notify,
-    get,
-    post,
-    put,
-    toObj,
-    toStr,
-    queryStr,
-    log,
-    done,
-  }
-}
