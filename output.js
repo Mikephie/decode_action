e8fb46a2efee14d9ae7238a3e7f1596295c910d7
action <action@github.com>
2025-04-14 00:51:18 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Mon Apr 14 2025 00:28:17 GMT+0000 (Coordinated Universal Time)
+//Mon Apr 14 2025 00:51:18 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 let names = "DailyWallpaper";
