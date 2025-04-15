62ec4800db56772f91a7414aa9e065ed02a7b7f5
action <action@github.com>
2025-04-15 23:59:50 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,6 +1,24 @@
-//Tue Apr 15 2025 22:39:20 GMT+0000 (Coordinated Universal Time)
+//Tue Apr 15 2025 23:59:50 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
+/*************************************
+
+>「 脚本名称 」         壁纸解锁Svip、Vip、无限涂鸦币
+>「 脚本作者 」         M̆̈̆̈ĭ̈̆̈k̆̈̆̈ĕ̈
+>「 电报频道 」         https://t.me/TrollStoreKios 
+>「 更新时间 」         2025-01-09
+>「 注意事项 」         如需引用请注明出处，谢谢合作！
+>「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
+>「 额外说明 」         请勿传播或售卖此脚本
+
+[rewrite_local]
+^https?:\/\/leancloud\.emotionwp\.com\/.+\/(classes|batch\/save) url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/BiZhi.js
+
+[mitm]
+hostname = leancloud.emotionwp.com
+
+*************************************/
+
 var Mike = JSON.parse($response.body);
 const user = /classes\/wpf_account/;
 const xiazai = /classes/;
