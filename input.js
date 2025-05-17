797ce850c26c180d89074b8093bc2ab8d3889639
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-05-18 00:16:38 +0800

    Update input.js

--- input.js
+++ input.js
@@ -1,15 +1,6 @@
 /****************************
 
 #!name = 酷我音乐 & 酷我畅听
-#!desc = 〔 酷我音乐&酷我畅听 〕全功能破解
-#!author = 影子[https://www.napi.ltd]
-#!homepage = https://napi.ltd
-#!openUrl = https://napi.ltd
-#!tag = 酷我音乐,酷我畅听
-#!loon_version = 3.2.3(762)
-#!icon = https://static.napi.ltd/Image/KuWo.png
-#!date = 2025-05-13
-
 
 [Argument]
 QS = select,"Hi-Res音质","无损音质","超品音质","高品音质",tag=音质选择
