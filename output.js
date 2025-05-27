8679d116a980411bd635baf4a9b05c74aa258ea3
action <action@github.com>
2025-05-27 21:47:50 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,111 +1,21 @@
-//Tue May 27 2025 21:42:59 GMT+0000 (Coordinated Universal Time)
+//Tue May 27 2025 21:47:50 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-(function () {
-  let appVersion = null;
-  let obj = JSON.parse($response.body);
-  let $ = new Env(names);
-  obj.subscriber = {
-    non_subscriptions: {},
-    first_seen: "2024-03-08T04:44:30Z",
-    original_application_version: appVersion,
-    other_purchases: {
-      [productType]: {
-        price: {
-          amount: 0,
-          currency: "USD"
-        },
-        display_name: null,
-        purchase_date: "2024-03-08T04:44:44Z"
-      }
-    },
-    management_url: null,
-    subscriptions: {},
-    entitlements: {},
-    original_purchase_date: "2024-03-08T04:44:14Z",
-    original_app_user_id: "$RCAnonymousID:0400000000000000000000000000000",
-    last_seen: "2024-03-08T04:44:30Z"
+eval(function (p, a, c, k, e, d) {
+  e = function (c) {
+    return (c < a ? "" : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36));
   };
-  obj.subscriber.non_subscriptions[productType] = [{
-    id: "aaaaaaaaaa",
-    is_sandbox: false,
-    price: {
-      amount: 0,
-      currency: "USD"
-    },
-    display_name: null,
-    purchase_date: "2024-03-08T04:44:44Z",
-    original_purchase_date: "2024-03-08T04:44:44Z",
-    store: "app_store",
-    store_transaction_id: "280000000000000"
-  }];
-  $.notify("XiaoMao_" + names + " 执行成功！", "", "Nice!已解锁成功，可关掉此脚本。", "https://i.pixiv.re/img-original/img/2022/12/19/00/06/12/103718184_p0.png");
-  $done({
-    body: JSON.stringify(obj)
-  });
-  function Env(name) {
-    const isLoon = typeof $loon !== "undefined";
-    const isSurge = typeof $httpClient !== "undefined" && !isLoon;
-    const isQX = typeof $task !== "undefined";
-    const read = key => {
-      if (isLoon || isSurge) return $persistentStore.read(key);
-      if (isQX) return $prefs.valueForKey(key);
+  if (!''.replace(/^/, String)) {
+    while (c--) d[e(c)] = k[c] || e(c);
+    k = [function (e) {
+      return d[e];
+    }];
+    e = function () {
+      return '\\w+';
     };
-    const write = (key, value) => {
-      if (isLoon || isSurge) return $persistentStore.write(key, value);
-      if (isQX) return $prefs.setValueForKey(key, value);
-    };
-    const notify = (title = "XiaoMao", subtitle = "", message = "", url = "", url2 = url) => {
-      if (isLoon) $notification.post(title, subtitle, message, url);
-      if (isSurge) $notification.post(title, subtitle, message, {
-        url
-      });
-      if (isQX) $notify(title, subtitle, message, {
-        "open-url": url,
-        "media-url": url2
-      });
-    };
-    const get = (url, callback) => {
-      if (isLoon || isSurge) $httpClient.get(url, callback);
-      if (isQX) {
-        url.method = `GET`;
-        $task.fetch(url).then(resp => callback(null, {}, resp.body));
-      }
-    };
-    const post = (url, callback) => {
-      if (isLoon || isSurge) $httpClient.post(url, callback);
-      if (isQX) {
-        url.method = `POST`;
-        $task.fetch(url).then(resp => callback(null, {}, resp.body));
-      }
-    };
-    const put = (url, callback) => {
-      if (isLoon || isSurge) $httpClient.put(url, callback);
-      if (isQX) {
-        url.method = "PUT";
-        $task.fetch(url).then(resp => callback(null, {}, resp.body));
-      }
-    };
-    const toObj = str => JSON.parse(str);
-    const toStr = obj => JSON.stringify(obj);
-    const queryStr = obj => {
-      return Object.keys(obj).map(key => `${key}=${obj[key]}`).join("&");
-    };
-    const log = message => console.log(message);
-    const done = (value = {}) => $done(value);
-    return {
-      name,
-      read,
-      write,
-      notify,
-      get,
-      post,
-      put,
-      toObj,
-      toStr,
-      queryStr,
-      log,
-      done
-    };
+    c = 1;
   }
-})();
+  ;
+  while (c--) if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
+  return p;
+}('4s(4l(p,a,c,k,e,d){e=4l(c){4k(c<a?"":e(4u(c/a)))+((c=c%a)>35?4p.4r(c+29):c.4t(36))};4m(!\'\'.4n(/^/,4p)){4o(c--)d[e(c)]=k[c]||e(c);k=[4l(e){4k d[e]}];e=4l(){4k\'\\\\w+\'};c=1};4o(c--)4m(k[c])p=p.4n(4w 4q(\'\\\\b\'+e(c)+\'\\\\b\',\'g\'),k[c]);4k p}(\'2d(25(p,a,c,k,e,d){e=25(c){26(c<a?"":e(2J(c/a)))+((c=c%a)>35?2b.2L(c+29):c.2K(36))};27(!\\\'\\\'.28(/^/,2b)){2a(c--)d[e(c)]=k[c]||e(c);k=[25(e){26 d[e]}];e=25(){26\\\'\\\\\\\\w+\\\'};c=1};2a(c--)27(k[c])p=p.28(2c 2I(\\\'\\\\\\\\b\\\'+e(c)+\\\'\\\\\\\\b\\\',\\\'g\\\'),k[c]);26 p}(\\\'7 z="1P";7 23="1d";7 x="1c.1b.1a.1e";7 H=b;7 1g=11;7 1h=11;7 5=p.13($1r.k);7 $=1s I(z);5.K={P:{},1t:"g-e-j:f:R",1v:H,1q:{[x]:{Q:{O:0,N:"M",},L:b,U:"g-e-j:f:E",},},1p:b,1o:{},1n:{},16:"g-e-j:f:1m",1l:"$1k:1j",1i:"g-e-j:f:R",};5.K.P[x]=[{1u:"1f",17:18,Q:{O:0,N:"M",},L:b,U:"g-e-j:f:E",16:"g-e-j:f:E",1X:"1x",1R:"1S",},];$.r("1T"+z+" 1U！","","1V!1W，1Y。","24://i.1Z.20/J-21/J/22/12/19/1w/1Q/12/1O.1N");$t({k:p.14(5)});1y I(Z){2 8=w $1z!=="y";2 a=w $n!=="y"&&!8;2 9=w $o!=="y";2 F=(6)=>{3(8||a)h $S.F(6);3(9)h $10.1A(6)};2 D=(6,l)=>{3(8||a)h $S.D(6,l);3(9)h $10.1B(6,l)};2 r=(s="1C",q="",d="",1="",T=1)=>{3(8)$X.m(s,q,d,1);3(a)$X.m(s,q,d,{1});3(9)$r(s,q,d,{"1D-1":1,"1E-1":T})};2 C=(1,4)=>{3(8||a)$n.C(1,4);3(9){1.u=`1F`;$o.v(1).G((c)=>4(b,{},c.k))}};2 m=(1,4)=>{3(8||a)$n.m(1,4);3(9){1.u=`1G`;$o.v(1).G((c)=>4(b,{},c.k))}};2 B=(1,4)=>{3(8||a)$n.B(1,4);3(9){1.u="1H";$o.v(1).G((c)=>4(b,{},c.k))}};2 Y=(15)=>p.13(15);2 W=(5)=>p.14(5);2 V=(5)=>{h 1I.1J(5).1K((6)=>`${6}=${5[6]}`).1L("&")};2 A=(d)=>1M.A(d);2 t=(l={})=>$t(l);h{Z,F,D,r,C,m,B,Y,W,V,A,t,}}\\\',2M,2N,\\\'|2O|2P|27|2Q|2R|2S|2T|2U|2W|3c|2X|2Y|2Z|30|44|31|26||32|33|34|37|38|39|3a|2G|2H|2V|2F|3e|2n|2m|2l|2k|2h|2i|2o|2g|2f|2e|2j|2p|2E|2r|2q|2s|2t|2u|2v|2w|2x|2y|2z|2A|2B|2C|2D|3b|3d|3E|3f|41|40||3Z|3Y|45|3W|42|3V||3T|3S|3R|3Q|3P|3O|3U|43|47|46|4j|4i|4h|4g|4f|4d|4c|4b|2c|4a|4e|49|48|3N|25|3X|3L|3u|3g|3h|3i|3j|3k|3l|3m|3n|3o|3p|3M|3q|3r|3s|3t|3v|3K|3w|3x|3y|3z|3A|3B|3C|3D|3F|3G|3H|3I\\\'.3J(\\\'|\\\'),0,{}))\',62,4X,\'|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||4l|4k|4m|4n||4o|4p|4w|4s|4Y|4Z|50|51|52|53|54|55|56|57|58|59|5a|5b|5c|5d|5e|5f|5g|5h|5i|5j|5k|4V|5m|5n|4W|5q|5o|4U|4q|4u|4t|4r|62|4y|4z|4A|4B|4C|4D|4E|4x|4F|4T|4H|4I|4J|4G|4K|4L|4M|4N|||4O|4P|4Q|4R|4S|5l|5p|5L|5r|6f|6i|6d|6c|6b|6a|6g|69|67|66|65|64|63|61|68|6h|6k|6j|6v|6u|6t|6s|6r|6p|6o|6n|6m|6l|6q|4v|60|6e|5Y|5P|5s|5t|5u|5v|5w|5x|5y|5z|5A|5B|5C|5D|5Z|5E|5F|5G||5H|5I|5W|5V|5U|5T|5S|5R|5X|5Q|5O|5N|5M|5K|5J\'.4v(\'|\'),0,{}))', 62, 404, '||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||return|function|if|replace|while|String|RegExp|fromCharCode|eval|toString|parseInt|split|new|isLoon|129|url|const|callback|obj|key|let|title|03|null|resp|message|2024|08T04|body|value|post|httpClient|task|JSON|toStr|isQX|notify|url2|appVersion|268|44Z|write|get|names|log|read|undefined|productType|typeof|fetch|put|then|img|Env|subscriber|display_name|USD|currency|amount|non_subscriptions|price|30Z|persistentStore|isSurge|purchase_date|queryStr|subtitle|notification|done|name|aaaaaaaaaa|removeads|raven_pro|com|skyriser|bingwallpapers|notifyState|false|original_purchase_date|loon|stringify|parse|prefs|is_sandbox|ua|str|0400000000000000000000000000000|RCAnonymousID|original_app_user_id|method|14Z|entitlements|subscriptions|app_store|id|other_purchases|response|first_seen|original_application_version|00|last_seen|management_url|console|true|280000000000000|06||DailyWallpaper|103718184_p0|png|join|map|setValueForKey|keys|PUT|POST|GET|media|valueForKey|XiaoMao|Object|store_transaction_id|open|执行成功|XiaoMao_|productName|2022|original|toObj|re|https|pixiv|可关掉此脚本|store|已解锁成功|Nice'.split('|'), 0, {}));
