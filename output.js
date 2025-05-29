8b0d35b8fa08d821ff4384ba707ade0e00630757
action <action@github.com>
2025-05-29 14:20:50 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,9 +1,12 @@
-//Thu May 29 2025 14:19:40 GMT+0000 (Coordinated Universal Time)
+//Thu May 29 2025 14:20:50 GMT+0000 (Coordinated Universal Time)
 //解密脚本在此
 /*************************************
 
-
->「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
+>「 脚本名称 」         壁纸解锁Svip、Vip、无限涂鸦币
+>「 脚本作者 」         M̆̈̆̈ĭ̈̆̈k̆̈̆̈ĕ̈
+>「 电报频道 」         https://t.me/TrollStoreKios 
+>「 更新时间 」         2025-01-09
+本，或去设置媒体与购买项目处切换ID]
 >「 额外说明 」         请勿传播或售卖此脚本
 
 [rewrite_local]
