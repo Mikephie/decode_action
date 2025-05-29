4daa2b210eae9c1b080449748ff16a85ed01b27f
action <action@github.com>
2025-05-29 11:09:44 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,7 +1,12 @@
-//Thu May 29 2025 10:19:03 GMT+0000 (Coordinated Universal Time)
+//Thu May 29 2025 11:09:44 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
 /*************************************
+
+>「 脚本名称 」         壁纸解锁Svip、Vip、无限涂鸦币
+>「 脚本作者 」         M̆̈̆̈ĭ̈̆̈k̆̈̆̈ĕ̈
+>「 电报频道 」         https://t.me/TrollStoreKios 
+>「 更新时间 」         2025-01-09
 >「 注意事项 」         如需引用请注明出处，谢谢合作！
 >「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
 >「 额外说明 」         请勿传播或售卖此脚本
