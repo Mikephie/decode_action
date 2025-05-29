b2067423e3cd3c345a7f470283fe16a5e3197243
action <action@github.com>
2025-05-29 21:36:00 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,103 +1,30 @@
-//Thu May 29 2025 21:32:11 GMT+0000 (Coordinated Universal Time)
+//Thu May 29 2025 21:36:00 GMT+0000 (Coordinated Universal Time)
 //解密脚本在此
-let ddm = JSON.parse($response.body);
-const headers = $request?.headers || {};
-const ua = headers["User-Agent"] || headers["user-agent"] || "";
-const profileid = headers["adapty-sdk-profile-id"] || headers["ADAPTY-SDK-PROFILE-ID"] || "";
-const time = Date.now();
-const list = {
-  "Logo%20Maker": {
-    dy: "dypda",
-    id: "com.limepresso.lm.paid.subscription.pro_yearly_high",
-    bundle_id: "com.limepresso.logomaker"
-  },
-  Luminar: {
-    dy: "dypda",
-    id: "com.skylum.luminaripad.lifetime",
-    bundle_id: "com.skylum.luminaripad"
-  },
-  Genie: {
-    dy: "dypda",
-    id: "yearly_advanced_pro",
-    bundle_id: "co.appnation.geniechat"
-  },
-  "Flight%20Tracker": {
-    dy: "dypda",
-    id: "com.iaftt.flightplusfree.49.99year",
-    bundle_id: "com.iaftt.flightplusfree"
-  },
-  "$%7BPRODUCT_NAME%7D": {
-    dy: "dypda",
-    id: "com.iaftt.flightplusfree.49.99year",
-    bundle_id: "com.iaftt.flightplusfree"
-  },
-  AvA: {
-    dy: "dypda",
-    id: "momo_yearly_subs_pro",
-    bundle_id: "com.scaleup.dreame"
-  },
-  PlantApp: {
-    dy: "dypda",
-    id: "plantapp.lifetime.promoted.sub",
-    bundle_id: "com.scaleup.plantid"
-  },
-  KeyboardGPT: {
-    dy: "dypda",
-    id: "smart.keyboard.yearly.01",
-    bundle_id: "com.smart.keyboard"
-  },
-  SketchAR: {
-    dy: "dypda",
-    id: "tech.sketchar.subscription.yearly",
-    bundle_id: "tech.sketchar.ios"
-  },
-  universal: {
-    dy: "dypda",
-    id: "remotetv.yearly.01",
-    bundle_id: "com.universal.remotetv"
-  },
-  Lingvist: {
-    dy: "dypda",
-    id: "com.lingvist.unlimited_12_months.v11.full_1md_ft",
-    bundle_id: "ee.keel24.Lingvist"
-  },
-  ChatAI: {
-    dy: "dypda",
-    id: "chatai_yearly_ios",
-    bundle_id: "com.scaleup.chatai"
-  },
-  FacePlus: {
-    dy: "dypda",
-    id: "faceplus_yearly_subs_3dft_ios",
-    bundle_id: "com.scaleup.faceplus"
-  },
-  Batched: {
-    dy: "dypdba",
-    id: "com.advasoft.batched.premium_year",
-    bundle_id: "com.advasoft.batched"
-  }
-};
 var encode_version = "jsjiami.com.v5";
 if (typeof $rocket !== "undefined") {
-  function getBoxJSValue(_0x5ea269) {
+  function getBoxJSValue(_0x2e827e) {
     try {
-      if (typeof $persistentStore !== "undefined" && typeof $persistentStore.read === "function") {
-        const _0x3a43b4 = $persistentStore.read(_0x5ea269);
-        console.log("🔍 成功读取 BoxJS 值（$persistentStore）：" + _0x5ea269 + " = " + _0x3a43b4);
-        return _0x3a43b4;
-      } else {
-        if (typeof $prefs !== "undefined" && typeof $prefs.valueForKey === "function") {
-          const _0x5e0bbf = $prefs.valueForKey(_0x5ea269);
-          console.log("🔍 成功读取 BoxJS 值（$prefs）：" + _0x5ea269 + " = " + _0x5e0bbf);
-          return _0x5e0bbf;
+      {
+        if (typeof $persistentStore !== "undefined" && typeof $persistentStore.read === "function") {
+          const _0x5cf54c = $persistentStore.read(_0x2e827e);
+          console.log("🔍 成功读取 BoxJS 值（$persistentStore）：" + _0x2e827e + " = " + _0x5cf54c);
+          return _0x5cf54c;
         } else {
-          {
+          if (typeof $prefs !== "undefined" && typeof $prefs.valueForKey === "function") {
+            {
+              const _0xca4144 = $prefs.valueForKey(_0x2e827e);
+              console.log("🔍 成功读取 BoxJS 值（$prefs）：" + _0x2e827e + " = " + _0xca4144);
+              return _0xca4144;
+            }
+          } else {
             console.log("⚠️ 无法检测到可用的 BoxJS 环境！");
           }
         }
       }
-    } catch (_0x17880a) {
-      console.log("⚠️ 读取 BoxJS 配置失败：" + _0x17880a.message);
+    } catch (_0x4f3ab7) {
+      {
+        console.log("⚠️ 读取 BoxJS 配置失败：" + _0x4f3ab7.message);
+      }
     }
     return null;
   }
@@ -110,171 +37,178 @@ if (typeof $rocket !== "undefined") {
     $done();
   }
 }
-const premiumTemplate = {
-  id: "premium",
-  is_lifetime: false,
-  store: "app_store",
-  starts_at: "2024-01-23T09:09:09.000000+0000",
-  expires_at: "2099-09-09T09:09:09.000000+0000",
-  will_renew: true,
-  is_active: true,
-  is_in_grace_period: false,
-  activated_at: "2024-01-23T09:09:09.000000+0000",
-  renewed_at: "2024-01-23T09:09:09.000000+0000",
-  is_refund: false,
-  vendor_transaction_id: "490001271881589",
-  vendor_original_transaction_id: "490001271881589",
-  is_sandbox: false,
-  active_introductory_offer_type: "trial"
-};
-const receiptTemplate = {
+const receipt = {
   quantity: "1",
-  purchase_date_ms: "1706000949000",
-  expires_date: "2099-09-09 09:09:09 Etc/GMT",
+  purchase_date_ms: "1694250549000",
   is_in_intro_offer_period: "false",
-  transaction_id: "490001271881589",
-  is_trial_period: "true",
-  original_transaction_id: "490001271881589",
-  purchase_date: "2024-01-23 09:09:09 Etc/GMT",
+  transaction_id: "490001314520000",
+  is_trial_period: "false",
+  original_transaction_id: "490001314520000",
+  purchase_date: "2023-09-09 09:09:09 Etc/GMT",
+  product_id: yearlyid,
+  original_purchase_date_pst: "2023-09-09 02:09:10 America/Los_Angeles",
   in_app_ownership_type: "PURCHASED",
-  original_purchase_date_ms: "1706000949000",
-  expires_date_ms: "4092628149000"
+  original_purchase_date_ms: "1694250550000",
+  web_order_line_item_id: "490000123456789",
+  purchase_date_pst: "2023-09-09 02:09:09 America/Los_Angeles",
+  original_purchase_date: "2023-09-09 09:09:10 Etc/GMT"
 };
-for (const key in list) {
-  const safeKey = key.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-  if (new RegExp("^" + safeKey, "i").test(ua)) {
+const expirestime = {
+  expires_date: "2099-09-09 09:09:09 Etc/GMT",
+  expires_date_pst: "2099-09-09 06:06:06 America/Los_Angeles",
+  expires_date_ms: "4092599349000"
+};
+let anchor = false;
+let data;
+for (const i in list) {
+  const regex = new RegExp("^" + i, "i");
+  if (regex.test(ua) || regex.test(bundle_id)) {
     const {
-      dy,
+      cm,
+      hx,
       id,
       ids,
-      bundle_id
-    } = list[key];
-    let subscriptions = {};
-    let receiptdata = [];
-    switch (dy) {
-      case "dypda":
-        subscriptions[id] = Object.assign({}, premiumTemplate, {
-          vendor_product_id: id
-        });
-        receiptdata.push(Object.assign({}, receiptTemplate, {
-          product_id: id
-        }));
+      latest,
+      version
+    } = list[i];
+    const receiptdata = Object.assign({}, receipt, {
+      product_id: id
+    });
+    switch (cm) {
+      case "timea":
+        data = [Object.assign({}, receiptdata, expirestime)];
         break;
-      case "dypdb":
-        subscriptions[id] = Object.assign({}, premiumTemplate, {
-          vendor_product_id: id
-        });
-        if (ids) {
-          subscriptions[ids] = Object.assign({}, premiumTemplate, {
-            vendor_product_id: ids
-          });
-          receiptdata.push(Object.assign({}, receiptTemplate, {
-            product_id: ids
-          }));
-        }
-        receiptdata.push(Object.assign({}, receiptTemplate, {
+      case "timeb":
+        data = [receiptdata];
+        break;
+      case "timec":
+        data = [];
+        break;
+      case "timed":
+        data = [Object.assign({}, receiptdata, expirestime, {
+          product_id: ids
+        }), Object.assign({}, receiptdata, expirestime, {
           product_id: id
-        }));
+        })];
         break;
     }
-    if (/(analytics\/profiles|purchase\/app-store)/.test($request.url)) {
-      ddm = {
-        data: {
-          type: "adapty_purchase_app_store_original_transaction_id_validation_result",
-          id: profileid,
-          attributes: {
-            profile_id: profileid,
-            is_test_user: false,
-            segment_hash: "8245f974014fdf4c",
-            timestamp: time,
-            apple_validation_result: {
-              environment: "Production",
-              revision: "1726387136000_490001234567890_4",
-              appAppleId: 1560806510,
-              transactions: [{
-                productId: id,
-                storefront: "CHN",
-                originalTransactionId: "490001234567890",
-                expiresDate: "2099-09-09T09:09:09Z",
-                subscriptionGroupIdentifier: "20459405",
-                purchaseDate: "2024-01-23 09:09:09Z",
-                price: 0,
-                transactionId: "490001234567890",
-                currency: "CNY",
-                inAppOwnershipType: "PURCHASED"
-              }],
-              hasMore: false,
-              bundleId: bundle_id
-            },
-            subscriptions: subscriptions,
-            paid_access_levels: {
-              premium: Object.assign({}, premiumTemplate, {
-                vendor_product_id: id
-              })
-            }
-          }
+    if (hx.includes("hxpda")) {
+      ddm.receipt.in_app = data;
+      ddm.latest_receipt_info = data;
+      ddm.pending_renewal_info = [{
+        product_id: id,
+        original_transaction_id: "490001314520000",
+        auto_renew_product_id: id,
+        auto_renew_status: "1"
+      }];
+      ddm.latest_receipt = latest;
+    } else {
+      if (hx.includes("hxpdb")) {
+        ddm.receipt.in_app = data;
+      } else {
+        if (hx.includes("hxpdc")) {
+          const xreceipt = {
+            expires_date_formatted: "2099-09-09 09:09:09 Etc/GMT",
+            expires_date: "4092599349000",
+            expires_date_formatted_pst: "2099-09-09 06:06:06 America/Los_Angeles",
+            product_id: id
+          };
+          ddm.receipt = Object.assign({}, ddm.receipt, xreceipt);
+          ddm.latest_receipt_info = Object.assign({}, ddm.receipt, xreceipt);
+          ddm.status = 0;
+          ddm.auto_renew_status = 1;
+          ddm.auto_renew_product_id = id;
+          delete ddm.latest_expired_receipt_info;
+          delete ddm.expiration_intent;
         }
-      };
+      }
     }
-    if (/(receipt\/validate|purchase-containers)/.test($request.url)) {
-      ddm = {
-        data: {
-          type: "adapty_inapps_apple_receipt_validation_result",
-          id: profileid,
-          attributes: {
-            profile_id: profileid,
-            apple_validation_result: {
-              environment: "Production",
-              receipt: {
-                receipt_type: "Production",
-                bundle_id: bundle_id,
-                in_app: receiptdata,
-                original_purchase_date: "2024-01-23 09:09:09 Etc/GMT",
-                adam_id: 1560806510,
-                request_date: "2024-01-23 09:09:09 Etc/GMT",
-                request_date_ms: "1706000949000",
-                application_version: "1",
-                original_application_version: "1"
-              },
-              status: 0,
-              pending_renewal_info: [{
-                expiration_intent: "1",
-                product_id: id,
-                is_in_billing_retry_period: "0",
-                auto_renew_product_id: id,
-                original_transaction_id: "490001234567890",
-                auto_renew_status: "0"
-              }],
-              latest_receipt_info: receiptdata,
-              latest_receipt: "ddm"
-            },
-            subscriptions: subscriptions,
-            paid_access_levels: {
-              premium: Object.assign({}, premiumTemplate, {
-                vendor_product_id: id
-              })
-            }
-          }
-        }
-      };
+    if (version && version.trim() !== "") {
+      ddm.receipt.original_application_version = version;
     }
-    console.log("已操作成功🎉🎉🎉\n叮当猫の分享频道: https://t.me/ddm1023");
+    anchor = true;
+    console.log("恭喜您，已操作成功🎉🎉🎉\n叮当猫の分享频道: https://t.me/ddm1023");
     break;
   }
+}
+if (!anchor) {
+  data = [Object.assign({}, receipt, expirestime)];
+  ddm.receipt.in_app = data;
+  ddm.latest_receipt_info = data;
+  ddm.pending_renewal_info = [{
+    product_id: yearlyid,
+    original_transaction_id: "490001314520000",
+    auto_renew_product_id: yearlyid,
+    auto_renew_status: "1"
+  }];
+  ddm.latest_receipt = "ddm1023";
+  console.log("很遗憾未能识别出UA或bundle_id\n但已使用备用方案🎉🎉🎉\n叮当猫の分享频道: https://t.me/ddm1023");
 }
 $done({
   body: JSON.stringify(ddm)
 });
-(function (_0x13a141, _0x2c2753, _0x35c450) {
-  _0x35c450 = "al";
-  try {
-    _0x35c450 += "ert";
-    _0x2c2753 = encode_version;
-    if (!(typeof _0x2c2753 !== "undefined" && _0x2c2753 === "jsjiami.com.v5")) {
-      _0x13a141[_0x35c450]("删除版本号，js会定期弹窗，还请支持我们的工作");
+(function (_0x42d9e9, _0x383b4f, _0x2bdc5a) {
+  var _0x138f6b = function () {
+    var _0x5ce2f6 = true;
+    return function (_0x5c4a2a, _0x5f5325) {
+      var _0x5187d8 = _0x5ce2f6 ? function () {
+        if (_0x5f5325) {
+          var _0x55821e = _0x5f5325.apply(_0x5c4a2a, arguments);
+          _0x5f5325 = null;
+          return _0x55821e;
+        }
+      } : function () {};
+      _0x5ce2f6 = false;
+      return _0x5187d8;
+    };
+  }();
+  var _0x595456 = _0x138f6b(this, function () {
+    var _0x4d66c1 = function () {
+      return "dev";
+    };
+    var _0x4335e3 = function () {
+      return "window";
+    };
+    var _0x279e08 = function () {
+      var _0x2cc97b = new RegExp("\\w+ *\\(\\) *{\\w+ *['|\"].+['|\"];? *}");
+      return !_0x2cc97b.test(_0x4d66c1.toString());
+    };
+    var _0x44e0d3 = function () {
+      var _0x3411a2 = new RegExp("(\\\\[x|u](\\w){2,4})+");
+      return _0x3411a2.test(_0x4335e3.toString());
+    };
+    var _0x4a72b8 = function (_0x27d0af) {
+      var _0x129ce = 0 >> 1 + NaN;
+      if (_0x27d0af.indexOf("i" === _0x129ce)) {
+        _0xbab618(_0x27d0af);
+      }
+    };
+    var _0xbab618 = function (_0x2ac782) {
+      var _0x4d26e3 = 3 >> 1 + NaN;
+      if (_0x2ac782.indexOf("true"[3]) !== _0x4d26e3) {
+        _0x4a72b8(_0x2ac782);
+      }
+    };
+    if (!_0x279e08()) {
+      if (!_0x44e0d3()) {
+        _0x4a72b8("indеxOf");
+      } else {
+        _0x4a72b8("indexOf");
+      }
+    } else {
+      _0x4a72b8("indеxOf");
     }
-  } catch (_0x88cffb) {
-    _0x13a141[_0x35c450]("删除版本号，js会定期弹窗");
+  });
+  _0x595456();
+  _0x2bdc5a = "al";
+  try {
+    _0x2bdc5a += "ert";
+    _0x383b4f = encode_version;
+    if (!(typeof _0x383b4f !== "undefined" && _0x383b4f === "jsjiami.com.v5")) {
+      _0x42d9e9[_0x2bdc5a]("删除版本号，js会定期弹窗，还请支持我们的工作");
+    }
+  } catch (_0x29cc78) {
+    _0x42d9e9[_0x2bdc5a]("删除版本号，js会定期弹窗");
   }
 })(window);
 encode_version = "jsjiami.com.v5";
