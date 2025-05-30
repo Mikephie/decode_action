4f06bef61bde0c32bcbcf63acf988de2b5dae6cb
action <action@github.com>
2025-05-30 15:12:16 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,3 +1,3 @@
-//Fri May 30 2025 15:06:33 GMT+0000 (Coordinated Universal Time)
+//Fri May 30 2025 15:12:16 GMT+0000 (Coordinated Universal Time)
 //解密脚本在此
 ]; o=(ﾟｰﾟ)  =_=3; c=(ﾟΘﾟ) =(ﾟｰﾟ)-(ﾟｰﾟ); (ﾟДﾟ) =(ﾟΘﾟ)= (o^_^o)/ (o^_^o);(ﾟДﾟ)={ﾟΘﾟ: 
