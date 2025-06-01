e84ea9e751c25e035629691c883e6b34f873fdf2
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-06-01 13:24:49 +0800

    Update input.js

--- input.js
+++ input.js
@@ -1,5 +1,5 @@
 /*************************************
-111222333344555666
+111222333344555666777
 
 [rewrite_local]
 ^https?:\/\/leancloud\.emotionwp\.com\/.+\/(classes|batch\/save) url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/BiZhi.js
