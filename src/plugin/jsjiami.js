3c26c92050d09c69aa27dd562cd199c930d76267
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-06-01 21:02:10 +0800

    o

--- src/plugin/jsjiami.js
+++ src/plugin/jsjiami.js
@@ -5,14 +5,14 @@ import _generator from '@babel/generator'
 const traverse = _traverse.default
 const generator = _generator.default
 
-// Node环境动态运行，获取 _0x1715() 返回表
 function getStringTableFromCode(code) {
   let table = []
   try {
-    // 防止干扰，把jsjiami.com.v7换掉，避免二次混淆
-    let sandboxed = code.replace(/(jsjiami\.com\.v7)/g, '$1\n;globalThis._0x1715=_0x1715;')
-    // 只运行 _0x1715() 拿到结果
-    // eslint-disable-next-line no-eval
+    // 将 _0x1715 暴露到 globalThis
+    let sandboxed = code.replace(
+      /(jsjiami\.com\.v7)/g,
+      '$1\n;globalThis._0x1715=_0x1715;'
+    )
     globalThis._0x1715 = undefined
     eval(sandboxed)
     if (typeof globalThis._0x1715 === 'function') {
@@ -29,7 +29,7 @@ export default function jsjiamiPlugin(code) {
   }
   let ast = parse(code)
   let indexerName = null
-  // 主动查找索引函数名
+  // 查找索引函数
   traverse(ast, {
     VariableDeclarator(path) {
       if (t.isIdentifier(path.node.init) && path.node.init.name.startsWith('_0x') && path.node.id.name.startsWith('_0x')) {
@@ -44,7 +44,7 @@ export default function jsjiamiPlugin(code) {
     console.log('[jsjiami] 没识别到索引函数名');
     return code
   }
-  // 用运行时 eval 获取字符串表
+  // 获取字符串表
   const stringTable = getStringTableFromCode(code)
   if (!Array.isArray(stringTable) || stringTable.length < 10) {
     console.log('[jsjiami] 没获得字符串表');
@@ -71,6 +71,6 @@ export default function jsjiamiPlugin(code) {
     console.log('[jsjiami] 没有还原出任何字符串');
     return code
   }
-  // 生成新代码
+  // 输出新代码
   return generator(ast, { jsescOption: { minimal: true } }).code
 }
