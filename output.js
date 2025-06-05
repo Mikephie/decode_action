5fc30bb9f6a260d72b533d13c4f727e50d93f3b5
action <action@github.com>
2025-06-05 08:04:23 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//2025-06-05T07:19:29.930Z
+//2025-06-05T08:04:23.797Z
 //解密脚本在此
 const ddm = JSON.parse($response.body);
 const ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
