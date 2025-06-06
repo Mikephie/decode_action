cc0822e4fa22d45de2c152f8b1a2e3641b8572ea
action <action@github.com>
2025-06-06 08:30:08 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,110 +1,24 @@
-//2025-06-06T08:29:53.654Z
+//2025-06-06T08:30:08.187Z
 //解密脚本在此
-(function () {
-  let appVersion = null;
-  let obj = JSON.parse($response.body);
-  let $ = new Env(names);
-  obj.subscriber = {
-    non_subscriptions: {},
-    first_seen: "2024-03-08T04:44:30Z",
-    original_application_version: appVersion,
-    other_purchases: {
-      [productType]: {
-        price: {
-          amount: 0,
-          currency: "USD"
-        },
-        display_name: null,
-        purchase_date: "2024-03-08T04:44:44Z"
-      }
-    },
-    management_url: null,
-    subscriptions: {},
-    entitlements: {},
-    original_purchase_date: "2024-03-08T04:44:14Z",
-    original_app_user_id: "$RCAnonymousID:0400000000000000000000000000000",
-    last_seen: "2024-03-08T04:44:30Z"
-  };
-  obj.subscriber.non_subscriptions[productType] = [{
-    id: "aaaaaaaaaa",
-    is_sandbox: false,
-    price: {
-      amount: 0,
-      currency: "USD"
-    },
-    display_name: null,
-    purchase_date: "2024-03-08T04:44:44Z",
-    original_purchase_date: "2024-03-08T04:44:44Z",
-    store: "app_store",
-    store_transaction_id: "280000000000000"
-  }];
-  $.notify("XiaoMao_" + names + " 执行成功！", "", "Nice!已解锁成功，可关掉此脚本。", "https://i.pixiv.re/img-original/img/2022/12/19/00/06/12/103718184_p0.png");
-  $done({
-    body: JSON.stringify(obj)
-  });
-  function Env(name) {
-    const isLoon = typeof $loon !== "undefined";
-    const isSurge = typeof $httpClient !== "undefined" && !isLoon;
-    const isQX = typeof $task !== "undefined";
-    const read = key => {
-      if (isLoon || isSurge) return $persistentStore.read(key);
-      if (isQX) return $prefs.valueForKey(key);
-    };
-    const write = (key, value) => {
-      if (isLoon || isSurge) return $persistentStore.write(key, value);
-      if (isQX) return $prefs.setValueForKey(key, value);
-    };
-    const notify = (title = "XiaoMao", subtitle = "", message = "", url = "", url2 = url) => {
-      if (isLoon) $notification.post(title, subtitle, message, url);
-      if (isSurge) $notification.post(title, subtitle, message, {
-        url
-      });
-      if (isQX) $notify(title, subtitle, message, {
-        "open-url": url,
-        "media-url": url2
-      });
-    };
-    const get = (url, callback) => {
-      if (isLoon || isSurge) $httpClient.get(url, callback);
-      if (isQX) {
-        url.method = `GET`;
-        $task.fetch(url).then(resp => callback(null, {}, resp.body));
-      }
-    };
-    const post = (url, callback) => {
-      if (isLoon || isSurge) $httpClient.post(url, callback);
-      if (isQX) {
-        url.method = `POST`;
-        $task.fetch(url).then(resp => callback(null, {}, resp.body));
-      }
-    };
-    const put = (url, callback) => {
-      if (isLoon || isSurge) $httpClient.put(url, callback);
-      if (isQX) {
-        url.method = "PUT";
-        $task.fetch(url).then(resp => callback(null, {}, resp.body));
-      }
-    };
-    const toObj = str => JSON.parse(str);
-    const toStr = obj => JSON.stringify(obj);
-    const queryStr = obj => {
-      return Object.keys(obj).map(key => `${key}=${obj[key]}`).join("&");
-    };
-    const log = message => console.log(message);
-    const done = (value = {}) => $done(value);
-    return {
-      name,
-      read,
-      write,
-      notify,
-      get,
-      post,
-      put,
-      toObj,
-      toStr,
-      queryStr,
-      log,
-      done
-    };
-  }
-})();
+ﾟωﾟﾉ = /｀ｍ´）ﾉ ~┻━┻   /['_'];
+o = ﾟｰﾟ = _ = 3;
+c = ﾟΘﾟ = ﾟｰﾟ - ﾟｰﾟ;
+ﾟДﾟ = ﾟΘﾟ = (o ^ _ ^ o) / (o ^ _ ^ o);
+ﾟДﾟ = {
+  ﾟΘﾟ: '_',
+  ﾟωﾟﾉ: ((ﾟωﾟﾉ == 3) + '_')[ﾟΘﾟ],
+  ﾟｰﾟﾉ: (ﾟωﾟﾉ + '_')[o ^ _ ^ o - ﾟΘﾟ],
+  ﾟДﾟﾉ: ((ﾟｰﾟ == 3) + '_')[ﾟｰﾟ]
+};
+ﾟДﾟ[ﾟΘﾟ] = ((ﾟωﾟﾉ == 3) + '_')[c ^ _ ^ o];
+ﾟДﾟ['c'] = (ﾟДﾟ + '_')[ﾟｰﾟ + ﾟｰﾟ - ﾟΘﾟ];
+ﾟДﾟ['o'] = (ﾟДﾟ + '_')[ﾟΘﾟ];
+ﾟoﾟ = ﾟДﾟ['c'] + ﾟДﾟ['o'] + (ﾟωﾟﾉ + '_')[ﾟΘﾟ] + ((ﾟωﾟﾉ == 3) + '_')[ﾟｰﾟ] + (ﾟДﾟ + '_')[ﾟｰﾟ + ﾟｰﾟ] + ((ﾟｰﾟ == 3) + '_')[ﾟΘﾟ] + ((ﾟｰﾟ == 3) + '_')[ﾟｰﾟ - ﾟΘﾟ] + ﾟДﾟ['c'] + (ﾟДﾟ + '_')[ﾟｰﾟ + ﾟｰﾟ] + ﾟДﾟ['o'] + ((ﾟｰﾟ == 3) + '_')[ﾟΘﾟ];
+ﾟДﾟ['_'] = (o ^ _ ^ o)[ﾟoﾟ][ﾟoﾟ];
+ﾟεﾟ = ((ﾟｰﾟ == 3) + '_')[ﾟΘﾟ] + ﾟДﾟ.ﾟДﾟﾉ + (ﾟДﾟ + '_')[ﾟｰﾟ + ﾟｰﾟ] + ((ﾟｰﾟ == 3) + '_')[o ^ _ ^ o - ﾟΘﾟ] + ((ﾟｰﾟ == 3) + '_')[ﾟΘﾟ] + (ﾟωﾟﾉ + '_')[ﾟΘﾟ];
+ﾟｰﾟ += ﾟΘﾟ;
+ﾟДﾟ[ﾟεﾟ] = '\\';
+ﾟДﾟ.ﾟΘﾟﾉ = (ﾟДﾟ + ﾟｰﾟ)[o ^ _ ^ o - ﾟΘﾟ];
+oﾟｰﾟo = (ﾟωﾟﾉ + '_')[c ^ _ ^ o];
+ﾟДﾟ[ﾟoﾟ] = '\"';
+ﾟДﾟ['_'](ﾟДﾟ['_'](ﾟεﾟ + ﾟДﾟ[ﾟoﾟ] + ﾟДﾟ[ﾟεﾟ] + ﾟΘﾟ + (ﾟｰﾟ + ﾟΘﾟ) + (ﾟｰﾟ + ﾟΘﾟ) + ﾟДﾟ[ﾟεﾟ] + ﾟΘﾟ + (ﾟｰﾟ + ﾟΘﾟ) + ﾟΘﾟ + ﾟДﾟ[ﾟεﾟ] + ﾟΘﾟ + (ﾟｰﾟ + ﾟΘﾟ) + (o ^ _ ^ o) + ﾟДﾟ[ﾟεﾟ] + ﾟΘﾟ + ﾟｰﾟ + (ﾟｰﾟ + ﾟΘﾟ) + ﾟДﾟ[ﾟεﾟ] + ﾟΘﾟ + ((o ^ _ ^ o) + (o ^ _ ^ o)) + (c ^ _ ^ o) + ﾟДﾟ[ﾟεﾟ] + ﾟΘﾟ + (ﾟｰﾟ + ﾟΘﾟ) + (c ^ _ ^ o) + ﾟДﾟ[ﾟεﾟ] + ﾟΘﾟ + (ﾟｰﾟ + ﾟΘﾟ) + ﾟΘﾟ + ﾟДﾟ[ﾟεﾟ] + ﾟΘﾟ + ﾟｰﾟ + (ﾟｰﾟ + ﾟΘﾟ) + ﾟДﾟ[ﾟoﾟ])(ﾟΘﾟ))('_');
