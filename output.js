a3a66d74d903c168aa86956f50208360e4eaaa7a
action <action@github.com>
2025-07-29 10:56:41 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,51 +1,48 @@
-//2025-07-21T06:00:43.155Z
+//2025-07-29T10:56:41.540Z
 //解密脚本在此
-if ($request.url.indexOf("/strategy/listen-url/") != -1) {
-  var _0x27b8b4 = $request.headers;
-  _0x27b8b4.uid = "91414537623";
-  _0x27b8b4.Cookie = "";
-  _0x27b8b4.ce = "";
-  _0x27b8b4.bversionid = "";
-  $done({
-    "headers": _0x27b8b4
-  });
+var body = $response.body;
+var url = $request.url;
+const p1 = "/api/v1/user/info?";
+const p2 = "/v1/short/play";
+const p3 = "/api/v1/payment/apple/pay/verify/info?";
+if (typeof body === "string") {
+  if (url.indexOf(p2) !== -1) {
+    body = body.replace(/locked":\d/g, "locked\" :0");
+  }
+  var obj;
+  try {
+    obj = JSON.parse(body);
+    if (url.indexOf(p1) !== -1) {
+      obj.result.isVip = 1;
+      obj.result.isForeverVip = 1;
+      obj.result.vipExpireTime = 32493834549000;
+      obj.result.bizId = 20000;
+      body = JSON.stringify(obj);
+    }
+    if (url.indexOf(p3) !== -1) {
+      obj = {
+        "message": {
+          "messageInfo": "H20000",
+          "serverTime": 1733924690381,
+          "code": 200
+        },
+        "result": {
+          "unsign": 1,
+          "agreementNo": "000001899416154",
+          "isTrialPeriod": false,
+          "eventToken": null,
+          "expiresDate": 32493834549000,
+          "isVip": 1,
+          "skuType": null
+        }
+      };
+      body = JSON.stringify(obj);
+    }
+  } catch (_0x37d1d6) {
+    console.log("JSON 解析错误: " + _0x37d1d6.message);
+  }
 }
-if ($request.url.indexOf("/music/batchQueryMusicPolicy.do") != -1) {
-  var _0x5e6bfc = $request.headers;
-  _0x5e6bfc.uid = "91414537623";
-  _0x5e6bfc.Cookie = "";
-  _0x5e6bfc.ce = "";
-  _0x5e6bfc.bversionid = "";
-  $done({
-    "headers": _0x5e6bfc
-  });
-} else if ($request.url.indexOf("user/api/my-page-header/") != -1) {
-  var _0x264071 = JSON.parse($response.body);
-  _0x264071.data.userIdentityIconItems[0].iconUrl = "https://d.musicapp.migu.cn/prod/file-service/file-down/bcb5ddaf77828caee4eddc172edaa105/7cd657454195aaeaea9e3c425491a0d0/74f77a7b9a47582a559762111ac8ba9b";
-  _0x264071.data.userIdentityIconItems[0].name = "白金会员畅听版";
-  _0x264071.data.userIdentityIconItems[0].type = "baijinhuiyuanchangtingban";
-  _0x264071.data.location = "https://t.me/GieGie777";
-  _0x264071.data.userOpNums[5].desc = "999999";
-  _0x264071.data.userOpNums[5].number = 999999;
-  var _0x50ab8a = JSON.stringify(_0x264071);
-  $done({
-    "body": _0x50ab8a
-  });
-} else if ($request.url.indexOf("/column/startup-pic-with-ad") != -1) {
-  var _0x33dad8 = JSON.parse($response.body);
-  delete _0x33dad8.data;
-  var _0x355684 = JSON.stringify(_0x33dad8);
-  $done({
-    "body": _0x355684
-  });
-} else if ($request.url.indexOf("resource/skin/list") != -1) {
-  var _0x33dad8 = JSON.parse($response.body);
-  _0x33dad8.data[0].isVip = "0";
-  var _0x355684 = JSON.stringify(_0x33dad8);
-  $done({
-    "body": _0x355684
-  });
-} else {
-  $done({});
-}
+$done({
+  "body": body
+});
 encode_version = "jsjiami.com.v5";
