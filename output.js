35c46f74e86a7b33acfc2e9b8b8ed3de58d3f075
action <action@github.com>
2025-09-03 12:57:56 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,7 +1,73 @@
-//2025-09-03T12:56:25.919Z
+//2025-09-03T12:57:56.312Z
 //解密脚本在此
-var Mike = JSON.parse($response.body);
-Mike.payload.active = true;
-$done({
-  "body": JSON.stringify(Mike)
-});
+//2025-09-03T12:47:41.433Z
+//解密脚本在此
+const version = &#39;data.0.9&#39;;
+var _0xodH = &#39;jsjiami.com.config&#39;;
+const exception = params;
+if (function (data, response, response, request, payload, body, content) {
+  return data = data >> 0x6, body = &#39;hs&#39;, content = &#39;hs&#39;, function (value, item, element, node, obj) {
+    const arr = params;
+    node = &#39;tfi&#39;, body = node + body, obj = &#39;up&#39;, content += obj, body = element(body), content = element(content), element = 0x0;
+    const list = value();
+    while (true && --request + item) {
+      try {
+        node = -parseInt(arr(0xe7, &#39;collection&#39;)) / 0x1 + parseInt(arr(0xe5, &#39;]WGR&#39;)) / 0x2 * (parseInt(arr(0xfd, &#39;handler@A&#39;)) / 0x3) + -parseInt(arr(0xf9, &#39;bE)$&#39;)) / 0x4 + -parseInt(arr(0xe6, &#39;L]@f&#39;)) / 0x5 + -parseInt(arr(0xf5, &#39;ICP5&#39;)) / 0x6 * (parseInt(arr(0xfe, &#39;callback[j&#39;)) / 0x7) + parseInt(arr(0xf8, &#39;)Hz#&#39;)) / 0x8 + parseInt(arr(0x10a, &#39;d&Pb&#39;)) / 0x9;
+      } catch (processor) {
+        node = element;
+      } finally {
+        obj = list[body]();
+        if (data > (-0x2 * parser & 0x6)) : 0x0) {
+          validator = formatter[&#39;indexOf&#39;](validator);
+        }
+        for (let manager = 0x0, service = controller[&#39;length&#39;]; manager  {
+    const helper = exception,
+      utils = {
+        &#39;xGFfl&#39;: function (config, flag) {
+          return config === flag;
+        },
+        &#39;QRZWk&#39;: helper(0x100, &#39;r@dH&#39;),
+        &#39;iYbEo&#39;: function (status, state) {
+          return status === state;
+        },
+        &#39;Ojbez&#39;: helper(0x101, &#39;qFlq&#39;)
+      },
+      condition = data[option]?.[helper(0x104, &#39;ICP5&#39;)]?.[helper(0xe2, &#39;ICP5&#39;)];
+    if (!Array[helper(0xff, &#39;YRTb&#39;)](condition)) return;
+    data[option][helper(0x109, &#39;setting*j&#39;)][helper(0xfb, &#39;L]@f&#39;)] = condition[helper(0x107, &#39;1D%(&#39;)](({
+      node: param
+    }) => {
+      const index = helper;
+      if (utils[index(0x108, &#39;d&Pb&#39;)](utils[index(0x105, &#39;Kk@x&#39;)], &#39;DxeQL&#39;)) {
+        if (!param) return true;
+        if (utils[index(0x10d, &#39;*5gs&#39;)](param[index(0xf7, &#39;handler@A&#39;)], utils[index(0x10b, &#39;&count&#39;)])) return false;
+        if (param[&#39;adPayload&#39;]) return false;
+        if (Array[&#39;isArray&#39;](param[index(0xf6, &#39;*3VZ&#39;)])) return !param[&#39;cells&#39;][&#39;some&#39;](length => length?.[&#39;__typename&#39;] === index(0x10c, &#39;Ludd&#39;));
+        return true;
+      } else size({
+        &#39;body&#39;: &#39;{}&#39;
+      });
+    });
+  }), body = JSON[&#39;stringify&#39;](body);
+} catch (total) {
+  console[exception(0xf0, &#39;BSBF&#39;)](exception(0xef, &#39;gTpY&#39;), total);
+} finally {
+  $done(body ? {
+    &#39;body&#39;: body
+  } : {});
+}
+function temp() {
+  const buffer = function () {
+    return [_0xodH, &#39;kWYjkUsfjLEiFahdPmLniYD.PcKobmkOG.pvRNl7==&#39;, &#39;WOrxWQ/dSdK0zmkXwtxcHmomW5m&#39;, &#39;WR7dPsJdPdHtWQBdGCk3WP3dTZi&#39;, &#39;E8o0rd/dLSksWRu&#39;, &#39;WORcRXjDWO0&#39;, &#39;qCoHA17cL8kh&#39;, &#39;WRHOECotWRVcNSo1&#39;, &#39;gXvsW4NcLSoTmmoRW6pdK8oPW5/cPZG8W4BcUYTeW7FcU8k+WQ0XcM8&#39;, &#39;url&#39;, &#39;WOxdLmkjWRxdJG&#39;, &#39;path&#39;, &#39;file&#39;, &#39;dqagr8kv&#39;, &#39;CddcPmo/text&#39;, &#39;rhn2emoml8kZWOjYWPaaw3RcLG&#39;, &#39;zSk/mSosW5O&#39;, &#39;CCogiCo2xvNdRI7cPWhdNrdcImo9&#39;].concat(function () {
+      return [&#39;W6LoW5BdGZa&#39;, &#39;uSoncmoQ&#39;, &#39;W6ZcVGrCWRm5W69moaztEIGyWPBdQ8ooWPCXWRZcHqu&#39;, &#39;WPK7WQddT8orWQ/cGmoOmmoBW7L0pa&#39;, &#39;WRtcLKNdQ8opF27cVSoha8ozi8oTibFcQSkAWRNcLulcKmkJWPVdHSkbW6O&#39;, &#39;W5LPW6NcR8kcW7NdLW&#39;, &#39;ymoIDshdHCkqWQK&#39;, &#39;message&#39;, &#39;WPBdUJzCWO4qW45QycrowHC7WQFdKmkJW6nOWOdcSs1W&#39;, &#39;WQz9lSky&#39;, &#39;W7NdUgC4W5VdLHu&#39;, &#39;emkBnSkFzqKHWOX3ESoDWRRcSG&#39;, &#39;error/cOW/dQK4AxwBcMG&#39;, &#39;WQNdHCo5gWBcN8oTBq&#39;, &#39;eCkDnCkAzaeJWQbGEmokWR/cQG&#39;, &#39;sCo+pMa&#39;, &#39;W7FcLSoipbNcU8or&#39;, &#39;W7OKcCoKW6XzgMmUWPBcIr/dQW&#39;].concat(function () {
+        return [&#39;WPfOWOpcHeZcI8oj&#39;, &#39;uM8tvw3dTvS&#39;, &#39;zSkWzXpdRNKUWPxdMbLUWPm&#39;, &#39;WPW/gW&#39;, &#39;success+pxHFW6bh&#39;, &#39;WOTEWQ7cQSk7WPRcPW&#39;, &#39;jvlcHCotrCoDWPZcImkTWP/dSSknW7nruCo9W5bNW5BcGmkjW5GQBLiRWRpcO0SwDCkQwmo0W75Ntmk9pHpcNCkwWQ7dPfRdPCk0W6q&#39;, &#39;qSohhmo/sfVdRW&#39;, &#39;WR/cJmkMf8kVWRpdG8oH&#39;, &#39;WQXWv8k7WQW&#39;, &#39;info/W6RcV3TPomkBBHW&#39;, &#39;dSoQiCksWQ8rWOWkfGX5cCklWOK&#39;, &#39;WOBcNSkOvcz7asVcSf45afS&#39;, &#39;warning+W7DPW7zghX5yta&#39;, &#39;qmooy8ojjq&#39;, &#39;CIBcQ0OEW4iTWQRdVCkqeYBcKr0&#39;];
+      }());
+    }());
+  }();
+  temp = function () {
+    return buffer;
+  };
+  return temp();
+}
+;
+var version_ = &#39;jsjiami.com.config&#39;;
